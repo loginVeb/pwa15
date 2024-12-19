bdfcc5db657d5422239e3e0d8553b36e636a9ba6
loginVeb <loginVeb@yandex.ru>
2024-12-19 17:33:16 +0300

    ref linghthouse rab

--- app/viewport.js
+++ app/viewport.js
@@ -1,8 +1,6 @@
 export function generateViewport() {
     return {
       width: 'device-width',
-      initialScale: 1,
-      minimumScale: 1,
-      maximumScale: 1,
+      initialScale: 1
     }
   }
